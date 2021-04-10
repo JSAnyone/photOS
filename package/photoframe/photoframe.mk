f64f64832fc4a08feb024c0d1f79abda862da823
JSAnyone <74502558+JSAnyone@users.noreply.github.com>
2021-04-10 18:32:46 +0200

    added missing adjustments

--- package/photoframe/photoframe.mk
+++ package/photoframe/photoframe.mk
@@ -8,8 +8,9 @@ PHOTOFRAME_LICENSE = GPL-3.0+
 
 define PHOTOFRAME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/photoframe.sh $(TARGET_DIR)/usr/bin/photoframe.sh
-	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S79firstboot $(TARGET_DIR)/etc/init.d/S79firstboot
-	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S80photoframe $(TARGET_DIR)/etc/init.d/S80photoframe
+	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/usb_share.py $(TARGET_DIR)/usr/bin/usb_share.py
+	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S78firstboot $(TARGET_DIR)/etc/init.d/S78firstboot
+	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S79usb_share $(TARGET_DIR)/etc/init.d/S79usb_share
 	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/etc_photoframe/davfs2.conf $(TARGET_DIR)/etc/photoframe/davfs2.conf
 	$(INSTALL) -D -m 0600 $(PHOTOFRAME_PKGDIR)/etc_photoframe/photoframe.conf $(TARGET_DIR)/etc/photoframe/photoframe.conf
 	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/images/noimages.png $(TARGET_DIR)/usr/share/photoframe/noimages.png
